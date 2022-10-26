a9b9884b1ef7b185902f10ee74a49d5948a075bb
UmberX <orbricio2803@gmail.com>
2022-10-26 17:18:09 -0500

    Desafio Celular

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456-458", modelo: "Modelo 1", imei: "1111222355555", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "4987", modelo: "Modelo 2", imei: "2222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
